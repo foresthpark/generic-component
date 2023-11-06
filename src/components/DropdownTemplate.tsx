78446e4c86ab3c96860db0f7b370e8254e7066f6
Forest Park <foresthpark@gmail.com>
2023-11-05 22:39:20 -0800

    Update dropdown template with label and id attributes

--- src/components/DropdownTemplate.tsx
+++ src/components/DropdownTemplate.tsx
@@ -14,8 +14,13 @@ export function DropdownTemplate<T>({
   };
 
   return (
-    <div className="border-lg select-none border-[1px] border-green-400 p-1">
+    <>
+      <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+        Select an option
+      </label>
       <select
+        id="countries_disabled"
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
         value={selectedOption as string}
         onChange={(e) => handleOptionSelect(e.target.value as T)}
       >
@@ -28,6 +33,6 @@ export function DropdownTemplate<T>({
           </option>
         ))}
       </select>
-    </div>
+    </>
   );
 }
