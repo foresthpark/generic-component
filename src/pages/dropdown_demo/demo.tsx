9846f8e2bf7cd9e8728129cbd5fe7c4af0c51614
Forest Park <foresthpark@gmail.com>
2023-11-05 22:07:35 -0800

    Add dropdown template component and demo page

---
+++ src/pages/dropdown_demo/demo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DropdownTemplate } from "~/components/DropdownTemplate";
+
+export default function DropdownDemo() {
+  return (
+    <div className="h-full w-full p-4">
+      <DropdownTemplate />
+    </div>
+  );
+}
