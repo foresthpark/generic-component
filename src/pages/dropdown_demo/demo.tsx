df8383ee53068307be65b8e785c99ca9c61f3bff
Forest Park <foresthpark@gmail.com>
2023-11-05 22:32:20 -0800

    Refactor DropdownDemo component to use DropdownTemplate component with string and number options

--- src/pages/dropdown_demo/demo.tsx
+++ src/pages/dropdown_demo/demo.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { DropdownTemplate } from "~/components/DropdownTemplate";
 
 export default function DropdownDemo() {
+  const stringOptions: string[] = ["Option 1", "Option 2", "Option 3"];
+  const numberOptions: number[] = [1, 2, 3];
+
+  const [selectedStringOption, setSelectedStringOption] = useState<
+    string | null
+  >(null);
+  const [selectedNumberOption, setSelectedNumberOption] = useState<
+    number | null
+  >(null);
+
   return (
-    <div className="h-full w-full p-4">
-      <DropdownTemplate />
+    <div className="flex h-full w-full gap-10 p-4">
+      <div>
+        <h1>String Dropdown</h1>
+        <DropdownTemplate
+          options={stringOptions}
+          selectedOption={selectedStringOption}
+          onSelect={(option) => setSelectedStringOption(option)}
+        />
+      </div>
+
+      <div>
+        <h1>Number Dropdown</h1>
+        <DropdownTemplate
+          options={numberOptions}
+          selectedOption={selectedNumberOption}
+          onSelect={(option) => setSelectedNumberOption(option)}
+        />
+      </div>
     </div>
   );
 }
