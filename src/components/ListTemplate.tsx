de5ca4e16044e09d1eb3418df6bd0c6ea797caaf
Forest Park <foresthpark@gmail.com>
2023-11-13 22:43:03 -0800

    Add ListTemplate and MainPage components, update Home and add ListPage

---
+++ src/components/ListTemplate.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ListTemplate() {
+  return <div>ListTemplate</div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,8 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import BulletPoint from "~/components/BulletPoint";
-import ListExample from "~/components/ListExample";
-import { dessertData, personData } from "~/constants/data";
+import MainPage from "~/components/MainPage";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Hello Sekai/世界/World/🌎</title>
-        <meta name="description" content="Hello Sekai/世界/World/🌎" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex h-screen flex-col items-center justify-center text-xl">
-        <span className="pb-3">Hello Sekai/世界/World/🌎</span>
-        <ul>
-          {personData.map((name) => (
-            <BulletPoint
-              data={name}
-              key={name.firstName}
-              render={(name) => (
-                <span>
-                  {name.firstName} {name.lastName} - {name.age}yo /{" "}
-                  {name.visits} visits
-                </span>
-              )}
-            />
-          ))}
-        </ul>
-        -----------------------------------------------
-        <ul>
-          {dessertData.map((dessert) => (
-            <BulletPoint
-              data={dessert}
-              key={dessert.name}
-              render={(dessert) => (
-                <span>
-                  {dessert.name} - {dessert.calories} calories /{" "}
-                  {dessert.popularity} points
-                </span>
-              )}
-            />
-          ))}
-        </ul>
-        <br />
-      </div>
-    </>
-  );
+  return <MainPage />;
 };
 
 export default Home;
