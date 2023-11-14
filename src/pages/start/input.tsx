05aa5abf352a76d791e89f79e29d291ceadb1d6a
Forest Park <foresthpark@gmail.com>
2023-11-13 23:49:14 -0800

    Refactor InputStartTemplate component and create InputStartPage

---
+++ src/pages/start/input.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import InputStartTemplate from "~/components/start/Input/InputStartTemplate";
+
+export default function InputStartPage() {
+  return <InputStartTemplate />;
+}
