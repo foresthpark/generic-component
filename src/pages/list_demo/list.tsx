8eb4296aa4dfb600463ecf8b8006918b71276b28
Forest Park <foresthpark@gmail.com>
2023-11-17 23:49:05 -0800

    Add personData and dessertData arrays

--- src/pages/list_demo/list.tsx
+++ src/pages/list_demo/list.tsx
@@ -1,6 +1,60 @@
 import Head from "next/head";
 import BulletPoint from "~/components/BulletPoint";
-import { dessertData, personData } from "~/constants/data";
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+};
+
+type Dessert = {
+  name: string;
+  calories: number;
+  sweetness: number;
+  popularity: number;
+};
+
+const personData: Person[] = [
+  {
+    firstName: "Forest",
+    lastName: "Park",
+    age: 24,
+    visits: 100,
+  },
+  {
+    firstName: "Reanu",
+    lastName: "Keeves",
+    age: 40,
+    visits: 40,
+  },
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 45,
+    visits: 20,
+  },
+];
+
+const dessertData: Dessert[] = [
+  {
+    name: "Frozen yoghurt",
+    calories: 159,
+    sweetness: 6,
+    popularity: 74,
+  },
+  {
+    name: "Apple Pie",
+    calories: 237,
+    sweetness: 8,
+    popularity: 98,
+  },
+  {
+    name: "Cupcake",
+    calories: 262,
+    sweetness: 16,
+    popularity: 39,
+  },
+];
 
 export default function ListPage() {
   return (
